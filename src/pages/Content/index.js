6161cf56ef30f4a679627ae8e8805108bf1680b7
contributionls <qqqproton@protonmail.com>
2019-11-09 06:16:25 +0800

    feat: add analysis

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import UserStorage from '../../configs/user-storage';
 import { getCurrentPage } from '../../utils/page';
 import { initFilterComment } from '../../utils/comment';
@@ -21,6 +22,35 @@ async function start() {
       }
     });
     init();
+
+    // Standard Google Universal Analytics code
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      // eslint-disable-next-line no-unused-expressions
+      (i[r] =
+        i[r] ||
+        function() {
+          (i[r].q = i[r].q || []).push(arguments);
+        }),
+        (i[r].l = 1 * new Date());
+      // eslint-disable-next-line no-unused-expressions
+      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m);
+    })(
+      window,
+      document,
+      'script',
+      'https://www.google-analytics.com/analytics.js',
+      'ga'
+    ); // Note: https protocol here
+
+    ga('create', 'UA-144863614-2', 'auto'); // Enter your GA identifier
+    ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+    debug('pageview');
   } else {
     debug('matters multer has turned off.');
   }
--- src/pages/Options/index.jsx
+++ src/pages/Options/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render } from 'react-dom';
 
@@ -5,3 +6,31 @@ import Options from './Options';
 import './index.css';
 
 render(<Options />, window.document.querySelector('#app-container'));
+
+// Standard Google Universal Analytics code
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  // eslint-disable-next-line no-unused-expressions
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  // eslint-disable-next-line no-unused-expressions
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  'script',
+  'https://www.google-analytics.com/analytics.js',
+  'ga'
+); // Note: https protocol here
+
+ga('create', 'UA-144863614-2', 'auto'); // Enter your GA identifier
+ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+ga('require', 'displayfeatures');
+ga('send', 'pageview'); // Specify the virtual path
